449e52d201abcef22f773978df572366218c63cf
bigretromike <bigretromike@monogatari.pl>
2017-01-18 12:45:12 +0100

    merge worker inside main sh

--- kagami-watchdog.sh
+++ kagami-watchdog.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+#
+# KAGAMI for Twitter
+# Author : BigRetroMike
+# GPLv2
+#
 
 while true
 do
-	/opt/kagami/kagami-worker.sh
+	/opt/kagami/kagami.sh daemon
 	sleep 60
 	if [ -f /tmp/kagami/kagami.kill ]
 	then
